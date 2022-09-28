baef525fee542be5d232be9b4028071304a390c1
Wilheim <willabraham1@gmail.com>
2022-09-28 11:56:45 +1000

    fixed search and register

--- src/components/agents/Search.jsx
+++ src/components/agents/Search.jsx
@@ -7,6 +7,7 @@ export default function Search() {
   //   const userInfo = parseJwt(token);
 
   const navigate = useNavigate();
+
   const [user, setUser] = useState({
     firstName: "",
     lastName: "",
@@ -15,93 +16,112 @@ export default function Search() {
     email: "",
   });
 
+  const [userData, setUserData] = useState({});
+
+  const [hasResult, setHasResult] = useState(false);
+
   function find() {
-    findEntry(user).then((response) => {
-      if (response.ok) {
-        alert("User Found!");
-      } else {
-        alert(response.status);
-      }
-    });
+    findEntry(user)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+        setHasResult(true);
+      });
   }
 
   return (
-    <form onClick={(e) => {
-      e.preventDefault()
-    }}>
-      <label htmlFor="">First Name:</label>
-      <input
-        type="text"
-        onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-        value={user.firstName}
-        required
-      />
-      <br />
-      <label htmlFor="">Last Name:</label>
-      <input
-        type="text"
-        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-        value={user.lastName}
-        required
-      />
-      <br />
-      <label htmlFor="">Date of Birth:</label>
-      <input
-        type="date"
-        onChange={(e) => setUser({ ...user, dateOfBirth: e.target.value })}
-        value={user.dateOfBirth}
-        required
-      />
-      <br />
-      <label htmlFor="">Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        value={user.email}
-      />
-      <br />
-      <label htmlFor="">Mobile Number:</label>
-      <input
-        type="phone"
-        onChange={(e) => setUser({ ...user, mobile: e.target.value })}
-        value={user.mobile}
-      />
-      <button onClick={() => {
-        find()
-        foundResult(user)
-      }}>Search</button>
-    </form>
+    <>
+      <form
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="">First Name:</label>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+          value={user.firstName}
+          required
+        />
+        <br />
+        <label htmlFor="">Last Name:</label>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+          value={user.lastName}
+          required
+        />
+        <br />
+        <label htmlFor="">Date of Birth:</label>
+        <input
+          type="date"
+          onChange={(e) => setUser({ ...user, dateOfBirth: e.target.value })}
+          value={user.dateOfBirth}
+          required
+        />
+        <br />
+        <label htmlFor="">Email:</label>
+        <input
+          type="email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          value={user.email}
+          disabled
+        />
+        <br />
+        <label htmlFor="">Mobile Number:</label>
+        <input
+          type="phone"
+          onChange={(e) => setUser({ ...user, mobile: e.target.value })}
+          value={user.mobile}
+          disabled
+        />
+        <button
+          onClick={() => {
+            find();
+          }}
+        >
+          Search
+        </button>
+      </form>
+
+      {hasResult ? <FoundResult userData={userData} /> : null}
+    </>
   );
 
-  function foundResult({user}) {
+  function FoundResult(props) {
+    const { userData } = props;
     return (
       <div>
         <h2>Results</h2>
-        <table style="width:100%">
-          <tr>
-            <th>Search Criteria</th>
-            <th>Customer Details</th>
-          </tr>
-          <tr>
-            <td>First Name</td>
-            <td>{user.firstName}</td>
-          </tr>
-          <tr>
-            <td>Last Name</td>
-            <td>{user.lastName}</td>
-          </tr>
-          <tr>
-            <td>Date of Birth</td>
-            <td>{user.dateOfBirth}</td>
-          </tr>
-          <tr>
-            <td>Email Address</td>
-            <td>{user.email}</td>
-          </tr>
-          <tr>
-            <td>Mobile Number</td>
-            <td>{user.mobile}</td>
-          </tr>
+        <table>
+          <thead>
+            <tr>
+              <th>Search Criteria</th>
+              <th>Customer Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>First Name</td>
+              <td>{userData.firstName}</td>
+            </tr>
+            <tr>
+              <td>Last Name</td>
+              <td>{userData.lastName}</td>
+            </tr>
+            <tr>
+              <td>Date of Birth</td>
+              <td>{userData.dateOfBirth}</td>
+            </tr>
+            <tr>
+              <td>Email Address</td>
+              <td>{userData.email}</td>
+            </tr>
+            <tr>
+              <td>Mobile Number</td>
+              <td>{userData.mobile}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
