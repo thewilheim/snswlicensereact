fe2030a57dad5339105d828e858ac3ec856c1cd6
guru-r1 <gurusanjeev.ravindrarajah@studytafensw.edu.au>
2022-09-28 11:12:43 +1000

    Added Results Table on Search.jsx

--- src/components/agents/Search.jsx
+++ src/components/agents/Search.jsx
@@ -11,6 +11,8 @@ export default function Search() {
     firstName: "",
     lastName: "",
     dateOfBirth: "",
+    mobile: "",
+    email: "",
   });
 
   function find() {
@@ -24,7 +26,9 @@ export default function Search() {
   }
 
   return (
-    <form>
+    <form onClick={(e) => {
+      e.preventDefault()
+    }}>
       <label htmlFor="">First Name:</label>
       <input
         type="text"
@@ -49,7 +53,57 @@ export default function Search() {
         required
       />
       <br />
-      <button onClick={() => find()}>Search</button>
+      <label htmlFor="">Email:</label>
+      <input
+        type="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        value={user.email}
+      />
+      <br />
+      <label htmlFor="">Mobile Number:</label>
+      <input
+        type="phone"
+        onChange={(e) => setUser({ ...user, mobile: e.target.value })}
+        value={user.mobile}
+      />
+      <button onClick={() => {
+        find()
+        foundResult(user)
+      }}>Search</button>
     </form>
   );
+
+  function foundResult({user}) {
+    return (
+      <div>
+        <h2>Results</h2>
+        <table style="width:100%">
+          <tr>
+            <th>Search Criteria</th>
+            <th>Customer Details</th>
+          </tr>
+          <tr>
+            <td>First Name</td>
+            <td>{user.firstName}</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>{user.lastName}</td>
+          </tr>
+          <tr>
+            <td>Date of Birth</td>
+            <td>{user.dateOfBirth}</td>
+          </tr>
+          <tr>
+            <td>Email Address</td>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <td>Mobile Number</td>
+            <td>{user.mobile}</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
