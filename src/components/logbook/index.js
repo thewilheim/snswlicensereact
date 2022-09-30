d31f41fd3d5f435a6681e00109a9f5db87b1d75d
Kieran Raymond <Kieran.Raymond10@gmail.com>
2022-09-30 14:25:06 +1000

    updated index.js

--- src/components/logbook/index.js
+++ src/components/logbook/index.js
@@ -1,7 +1,11 @@
 import Create from "./Create";
 import LayoutPage from "./LogbookLayout";
+import LogbookCard from "./LogbookCard";
+import LogbookEntries from "./LogbookEntries";
 
 export {
     Create,
     LayoutPage,
+    LogbookCard,
+    LogbookEntries
 }
