d8cdb0918d0c702c6cd71e5b5d35775c67ff99c5
Wilheim <willabraham1@gmail.com>
2022-10-04 09:33:32 +1100

    Added Users Style css file

--- src/components/users/login.jsx
+++ src/components/users/login.jsx
@@ -1,55 +1,45 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginAsync } from "../../web-services";
-
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-
   useEffect(() => {
     if (token) {
       navigate(-1);
     }
   }, []);
-
   function login() {
     loginAsync(username, password)
       .then((j) => navigate("/"))
       .catch((e) => alert(e.message));
   }
-
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white border-t border-blue-600 rounded shadow-lg shadow-blue-800/50 lg:max-w-md">
-        <h1 className="text-3xl font-semibold text-left text-black-700">
-          Login
-        </h1>
-
+    <div className="viewContainerMain">
+      <div className="viewContainerBorder">
+        <h1 className="headingContainer">Login</h1>
         <form className="mt-6" onSubmit={(e) => e.preventDefault()}>
           <div>
-            <label htmlFor="email" className="block text-sm text-black-800">
+            <label htmlFor="email" className="labelHeaderStyle">
               Username
             </label>
             <input
               type="email"
-              className="block w-full px-4 py-2 mt-2 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="inputContainer"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mt-4">
             <div>
-              <label
-                htmlFor="password"
-                className="block text-sm text-black-800"
-              >
+              <label htmlFor="password" className="labelHeaderContainer">
                 Password
               </label>
               <input
                 type="password"
-                className="block w-full px-4 py-2 mt-2 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="inputContainer"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
@@ -58,10 +48,7 @@ export default function Login() {
               Forget Password?
             </a>
             <div className="mt-6">
-              <button
-                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none"
-                onClick={login}
-              >
+              <button className="btn-red" onClick={login}>
                 Login
               </button>
             </div>
